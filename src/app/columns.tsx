acee993c5bcc9feeb684af6015597a7070331bb9
Anand Rajaram <anandrajaram21@gmail.com>
2024-02-04 02:46:03 +0530

    feat: make bare minimum dashboard

---
+++ src/app/columns.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+export type Video = {
+  VideoID: string;
+  Title: string;
+  Description: string;
+  PublishDate: Date;
+  ThumbnailURL: string;
+};
+
+export const columns: ColumnDef<Video>[] = [
+  {
+    header: "Thumbnail",
+    accessorKey: "ThumbnailURL",
+    cell: ({ row }) => {
+      const imgUrl = row.getValue("ThumbnailURL");
+      return (
+        <div>
+          <img src={imgUrl}></img>
+        </div>
+      );
+    },
+  },
+  {
+    header: "Video ID",
+    accessorKey: "VideoID",
+  },
+  {
+    header: "Title",
+    accessorKey: "Title",
+  },
+  {
+    header: "Description",
+    accessorKey: "Description",
+  },
+  {
+    header: "Publish Date",
+    accessorKey: "PublishDate",
+    cell: ({ row }) => {
+      const date = new Date(row.getValue("PublishDate"));
+      return <div>{date.toLocaleDateString()}</div>;
+    },
+  },
+];
